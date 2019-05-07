7c98f7f99dff85886dde5ebd15c5895e0c19d8a5
Trey Schneider <trey@codeschneider.com>
2019-05-07 13:10:43 -0400

    add DASHBOARD_CLIENT_SUBDOMAIN to onlyAllowOrigins

--- config/sockets.js
+++ config/sockets.js
@@ -4,22 +4,16 @@ const {
   PROTOCOL,
   DOMAIN,
   INGEST_CLIENT_SUBDOMAIN,
+  DASHBOARD_CLIENT_SUBDOMAIN
 } = process.env;
 
 module.exports.sockets = {
 
   onlyAllowOrigins: [
-    `${PROTOCOL}://${INGEST_CLIENT_SUBDOMAIN}.${DOMAIN}`
+    `${PROTOCOL}://${INGEST_CLIENT_SUBDOMAIN}.${DOMAIN}`,
+    `${PROTOCOL}://${DASHBOARD_CLIENT_SUBDOMAIN}.${DOMAIN}`
   ],
   adapter: '@sailshq/socket.io-redis',
   url: `redis://${REDIS_HOSTNAME}:${REDIS_CONTAINER_PORT}/0`,
-
-  /***************************************************************************
-  *                                                                          *
-  * Whether to expose a 'GET /__getcookie' route that sets an HTTP-only      *
-  * session cookie.  //TODO                                                       *
-  *                                                                          *
-  ***************************************************************************/
-
   grant3rdPartyCookie: true,
 };
