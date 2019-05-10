20eeb85e06a6ce69fe38fea2c169a8f3b54546e8
Trey Schneider <trey@codeschneider.com>
2019-05-10 11:54:39 -0400

    add DASHBOARD_CLIENT_SUBDOMAIN to onlyAllowOrigins for sockets

--- config/env/production.js
+++ config/env/production.js
@@ -11,6 +11,7 @@ const {
   PROTOCOL,
   DOMAIN,
   INGEST_CLIENT_SUBDOMAIN,
+  DASHBOARD_CLIENT_SUBDOMAIN
 } = process.env;
 
 module.exports = {
@@ -53,10 +54,12 @@ module.exports = {
   sockets: {
 
     onlyAllowOrigins: [
-      `${PROTOCOL}://${INGEST_CLIENT_SUBDOMAIN}.${DOMAIN}`
+      `${PROTOCOL}://${INGEST_CLIENT_SUBDOMAIN}.${DOMAIN}`,
+      `${PROTOCOL}://${DASHBOARD_CLIENT_SUBDOMAIN}.${DOMAIN}`
     ],
     adapter: '@sailshq/socket.io-redis',
     url: `redis://${REDIS_HOSTNAME}:${REDIS_CONTAINER_PORT}/0`,
+    grant3rdPartyCookie: true,
   },
 
   log: {
