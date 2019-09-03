d32a2bdbe4a7fa65d5928f69e3978c59d20f8312
Trey Schneider <trey@codeschneider.com>
2019-09-03 11:45:51 -0400

    traefik migration -- production.js & sockets.js changes

--- config/sockets.js
+++ config/sockets.js
@@ -3,17 +3,17 @@ const {
   REDIS_CONTAINER_PORT,
   PROTOCOL,
   DOMAIN,
-  INGEST_CLIENT_SUBDOMAIN,
-  DASHBOARD_CLIENT_SUBDOMAIN
+  INGEST_URL,
+  DASHBOARD_URL
 } = process.env;
 
 module.exports.sockets = {
 
   onlyAllowOrigins: [
-    `${PROTOCOL}://${INGEST_CLIENT_SUBDOMAIN}.${DOMAIN}`,
-    `${PROTOCOL}://${DASHBOARD_CLIENT_SUBDOMAIN}.${DOMAIN}`
+    `${PROTOCOL}://${INGEST_URL}`,
+    `${PROTOCOL}://${DASHBOARD_URL}`
   ],
   adapter: '@sailshq/socket.io-redis',
   url: `redis://${REDIS_HOSTNAME}:${REDIS_CONTAINER_PORT}/0`,
-  grant3rdPartyCookie: true,
+  grant3rdPartyCookie: true
 };
